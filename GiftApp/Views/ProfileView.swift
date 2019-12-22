6d44293264217623288e66d2380c661ef3de5d42
Michael Olson <michael.j.olson@duke.edu>
2019-12-22 16:40:06 -0700

    profile view - home

--- GiftApp/Views/ProfileView.swift
+++ GiftApp/Views/ProfileView.swift
@@ -11,60 +11,79 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View {
         VStack{
-            Image("profile")
-                .resizable()
-                .frame(width: 150.0, height: 150.0)
             
-            Text("Matthew Olson")
-                .font(.title)
-                .fontWeight(.black)
-                .foregroundColor(.primary)
-                .lineLimit(3)
-                .padding()
-            
-            Text("Balance: $25")
-                .font(.headline)
-                .foregroundColor(.primary)
-                .lineLimit(3)
-                .padding()
-            
-            HStack{
-                NavigationLink(destination: BankTransferView()) {
-                    Text("Transfer to Bank")
-                      .fontWeight(.bold)
-                      .font(.headline)
-                      .padding()
-                      .background(Color.purple)
-                      .cornerRadius(40)
-                      .foregroundColor(.white)
-                }
+            // Main Card
+            VStack {
                 
-                NavigationLink(destination: CardsView()) {
-                    Text("Add/Edit Cards")
-                      .fontWeight(.bold)
-                      .font(.headline)
-                      .padding()
-                      .background(Color.purple)
-                      .cornerRadius(40)
-                      .foregroundColor(.white)
+                // Profile Image and Name
+                HStack {
+                    Spacer()
+                    VStack {
+                        Image(systemName: "rectangle.stack.person.crop")
+                            .resizable()
+                            .frame(width: 150.0, height: 150.0)
+                        .padding()
+                        
+                        Text("Matthew Olson")
+                            .font(.title)
+                            .fontWeight(.black)
+                            .foregroundColor(.primary)
+                            .lineLimit(3)
+                            .padding(.bottom)
+                    }
+                    Spacer()
+                }.background(Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 1))
+                      
+                // Balance
+                HStack{
+                    Spacer()
+                    Text("Balance: $25")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .padding()
+                    Spacer()
                 }
-                
-                Button(action: {
-                    print("Action goes here")
-                }, label: {
-                    Text("Edit Account")
-                      .fontWeight(.bold)
-                      .font(.headline)
-                      .padding()
-                      .background(Color.purple)
-                      .cornerRadius(40)
-                      .foregroundColor(.white)
-                })
+                .background(Color(red: 220/255, green: 220/255, blue: 220/255, opacity: 1))
             }
-            .padding(.top, 20)
+            .cornerRadius(20)
+            .shadow(radius: 3, x: 0, y: 0)
             
+            
+            // Buttons
+            VStack{
+                
+                // Transfer to Bank Account
+                HStack{
+                    Spacer()
+                    NavigationLink(destination: BankTransferView()) {
+                        Text("transfer balance to bank")
+                            .fontWeight(.heavy)
+                            .font(.headline)
+                          .padding()
+                          .foregroundColor(.white)
+                    }
+                    Spacer()
+                }.background(Color(red: 209/255, green: 166/255, blue: 255/255, opacity: 1))
+                .cornerRadius(20)
+                    .padding(.all)
+                
+                // Add and Edit Cards Link
+                HStack {
+                    Spacer()
+                    NavigationLink(destination: CardsView()) {
+                        Text("add and edit cards")
+                          .fontWeight(.bold)
+                          .font(.headline)
+                          .padding()
+                          .foregroundColor(.white)
+                    }
+                    Spacer()
+                }
+                .background(Color(red: 209/255, green: 166/255, blue: 255/255, opacity: 1))
+                .cornerRadius(20)
+                    .padding([.leading, .bottom, .trailing])
+            }
             Spacer()
-            
         }
         .padding()
     }
