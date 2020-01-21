e3e9eade96db03bd28023206dc1f2c2211d51c63
Michael Olson <michael.j.olson@duke.edu>
2020-01-21 14:19:42 -0800

    the ultimate basket upgrade

--- GiftApp/Views/GiftView.swift
+++ GiftApp/Views/GiftView.swift
@@ -11,52 +11,107 @@ import SwiftUI
 struct GiftView: View {
     var gift: Gift
     
+    @State var showMessage = false
     var body: some View {
         
-           
-        VStack{
+        VStack {
             
-            // Location and Caption
+            // Header
+            HStack{
+                Image("mike")
+                    .resizable()
+                    .frame(width: 115, height: 115)
+                    .clipShape(Circle())
+                    .shadow(radius: 3)
+                    .padding(.leading,25)
+                Text(gift.fromId + "\nsent you a gift!")
+                    .font(.system(size: 22))
+                    .fontWeight(.medium)
+                    .padding(.leading, 3.0)
+                Spacer()
+            }.padding(.bottom, 5)
+        
+            //Card
             VStack{
-                HStack {
-                    Spacer()
-                
-                    VStack {
+                // Vendor + Icon / Caption
+                VStack{
+                    HStack(alignment: .center){
                         Text(gift.vendor)
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .padding(8.0)
-                        Text(gift.caption)
-                            .multilineTextAlignment(.center)
-                            .padding(.horizontal, 50.0)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .shadow(radius: 0.5)
+                            .foregroundColor(Color(red: 125/255, green: 125/255, blue: 130/255, opacity: 1))
+                        
+                        Spacer()
+                        
+                        Image(systemName: "rectangle")
+                            .resizable()
+                            .frame(width: 40, height: 30)
+                            .shadow(radius: 0.5)
+                            .foregroundColor(Color(red: 125/255, green: 125/255, blue: 130/255, opacity: 1))
                     }
-            
-                    Spacer()
-                
-                
+                    
+                    // Caption
+                    HStack{
+                        VStack{
+                            Text(gift.caption)
+                                .font(.footnote)
+                                .multilineTextAlignment(.leading)
+                                .lineLimit(5)
+                                .shadow(radius: 0.5)
+                            .foregroundColor(Color(red: 125/255, green: 125/255, blue: 130/255, opacity: 1))
+                            Spacer()
+                        }.frame(minHeight: 100.0, maxHeight: 100.0)
+                        Spacer()
+                    }
                 }
-            }
-            .padding(.vertical, 30)
-            .background(Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 1))
-
-            // Person and amount remaining
-            HStack {
-                Spacer()
+                    .padding(.top, 25.0)
+                    .padding(.horizontal, 25.0)
+                    .background(LinearGradient(gradient: Gradient(colors:
+                        [Color(red: 250/255, green: 250/255, blue: 250/255, opacity: 1), .white]),
+                    startPoint: .top,
+                    endPoint: .bottom))
+                    
+                // Remaining Balance
                 HStack{
-                    Text(gift.fromId)
-                    Text("•")
-                    Text("$" + gift.remainingBalance)
-                }.padding()
-                Spacer()
+                    if(showMessage){
+                        Text("$" + gift.remainingBalance)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                        .shadow(radius: 0.5)
+                        .padding(.vertical)
+                        .padding(.leading, 25.0)
+                    } else {
+                        Toggle(isOn: $showMessage){
+                            Text("Toggle to reveal amount.")
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color.white)
+                                .shadow(radius: 0.5)
+                                .padding(.vertical)
+                                .padding(.leading, 25.0)
+                        }
+                    }
+                    Spacer()
+                }
+                    .background(Color(red: 125/255, green: 125/255, blue: 130/255, opacity: 1))
             }
-            .background(Color(red: 220/255, green: 220/255, blue: 220/255, opacity: 1))
-        }.cornerRadius(20)
-        .shadow(radius: 3, x: 0, y: 0)
+                .background(Color.white)
+                .cornerRadius(20)
+                .shadow(radius: 2)
+        }
+            .padding(12)
+            .padding(.top, 14)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(radius: 3)
     }
 }
 
 struct GiftView_Previews: PreviewProvider {
     static var previews: some View {
-        GiftView(gift: Gift(id: 10, fromId: "+13035147424", vendor: "Chipotle", caption: "Tacos", remainingBalance: "10.00"))
+        GiftView(gift: Gift(id: 10, fromId: "+13035147424", vendor: "Chipotle", caption: "Hey man, I know you've been working really hard and are a little stressed out about getting the app done. This burrito is on me! 🥳", remainingBalance: "10.00"))
     }
 }
+
+
+//
